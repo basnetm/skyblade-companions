640b932254c2c1b66db73c081844bd3390ea4022
basnetm <104549814+basnetm@users.noreply.github.com>
2023-12-29 15:34:17 +0545

    main menu

---
+++ SkyBlade Companion/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+    
+{
+    public string newGameScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+}
