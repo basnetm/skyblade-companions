0d7fa8816a8d815b5dd6e11c8a4b7f4deaee8720
basnetm <104549814+basnetm@users.noreply.github.com>
2023-12-29 12:26:26 +0545

    level 1 done

---
+++ SkyBlade Companion/Assets/Scripts/camera/cameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    private PlayerController player;
+    public BoxCollider2D boundsBox;
+    private float halfWidth, halfHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player=FindAnyObjectByType<PlayerController>();
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            transform.position = new Vector3(
+                Mathf.Clamp(player.transform.position.x, boundsBox.bounds.min.x+halfWidth, boundsBox.bounds.max.x- halfWidth),
+               Mathf.Clamp(player.transform.position.y, boundsBox.bounds.min.y+halfHeight, boundsBox.bounds.max.y- halfHeight), transform.position.z);
+        }
+    }
+}
